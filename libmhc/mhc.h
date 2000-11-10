96f4f44c74c7ca42e1db2142b63dd9fb2deb74e6
kimura <kimura>
2000-11-10 06:25:20 +0000

    add comments

--- libmhc/mhc.h
+++ libmhc/mhc.h
@@ -1,5 +1,80 @@
 #ifndef _MHC_H
 #define _MHC_H
+/*
+ * MHCデータ読み出しライブラリ libmhc ヘッダファイル
+ *
+ * (C) 2000, Ken'ichirou Kimura <kimura@db.is.kyushu-u.ac.jp>
+ *
+ * 関数一覧
+ *
+ * ストリーム型I/Oの関数と使い方のイメージは同じ。
+ * readdir()辺に合わせてある。
+ *
+ *
+ * MHCD* openmhc(const char *home_dir, const char *year_month);
+ *   home_dir/year_month のMHCデータを読み込む準備をする。例えば
+ *   mhcd_ptr = openmhc("/home/kimura/Mail/schedule/","2000/10");
+ *
+ * mhcent* readmhc(MHCD*);
+ *   エントリを順次読み出す。
+ *
+ * void seekmhc(MHCD*,int loc);
+ *   次に読むエントリの場所をloc分エントリを進める。loc < 0なら戻る
+ *
+ * void rewindmhc(MHCD*);
+ *   次に読むエントリの場所を初期位置に戻す。
+ *
+ * int  closemhc(MHCD*);
+ *   終了処理。リソースを解放する。
+ *
+ * int  isschedule(const mhcent*,int year,int month,int day);
+ *   そのエントリがyear/month/dayのスケジュールかどうかチェック。
+ *   スケジュールであるならば1を返し、違うなら0を返す。
+ *
+ * MHC* OpenMHC(const char* home_dir, int year, int month);
+ *   home_dir/year/month に加えてintersectも読み込む。
+ *   さらに、dayでハッシュして保持する。
+ *
+ * mhcent* ReadMHC(MHC*);
+ *   エントリを順次読む。
+ *
+ * void SetMHC(MHC*,int day);
+ *   MHC* をdayに合わせる。これ以降、ReadMHC()で読まれるのはyear/month/day
+ *   のエントリ。
+ *
+ * void SeekMHC(MHC*,int);
+ *   MHC* を進める。
+ *
+ * void RewindMHC(MHC*);
+ *   MHC* を初期位置に戻す。
+ *
+ * int CloseMHC(MHC*);
+ *   終了処理。
+ *
+ * char* GetSubject(const mhcent*);
+ *   X-SC-Subject: を取り出す。
+ *
+ * int GetAlarm(const mhcent*);
+ *   X-SC-Alarm: の値を分単位で返す。
+ *
+ *
+ * $HOME/Mail/schedule/ にある、2000/10/1のエントリを取り出すには
+ *
+ *   MHC* mhc_ptr;
+ *   mhc_ent* ent_ptr;
+ *
+ *   mhc_ptr = OpenMHC("$HOME/Mail/schedule/",2000,10);
+ *   SetMHC(mhc_ptr,1);
+ *
+ *   while((ent_ptr = ReadMHC(mhc_ptr)) != NULL){
+ *      printf("Subject:%s\n",GetSubject(ent_ptr));
+ *   }
+ *  
+ *   CloseMHC(mhc_ptr);
+ *
+ * などとする。
+ *
+ */
 
 #define NUM_OF_ARRAY(a) (sizeof(a) / sizeof(a[0]))
 
