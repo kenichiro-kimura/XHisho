9ea1c20b1e4a7e90ab6af44fbe9edb873135b6cf
kimura <kimura>
2001-05-17 12:23:21 +0000

    remove kill(0,TERM) on exit()

--- main.c
+++ main.c
@@ -183,9 +183,6 @@ void Quit(Widget w, XEvent * event, String * params, unsigned int *num_params)
     /*while(wait(&i) != youbin_pid[0]);*/
   }
 
-#ifdef OPTION
-  kill(0,SIGTERM);
-#endif
   exit(0);
 }
 
