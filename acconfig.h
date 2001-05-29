c55cd27ece83fcb9777214633e66eb1e2c787960
kimura <kimura>
2001-05-29 09:01:21 +0000

    add some new entry

--- acconfig.h
+++ acconfig.h
@@ -18,3 +18,22 @@
 
 /* Define PREFIX */
 #undef PREF
+
+/* Define for libmhc :-) */
+#undef LIBMHC
+
+/* Define if you want to use XADL.  */
+#undef XADL
+
+/* Define if you want to use 'Something except that with option'.  */
+#undef OPTION
+
+/* Define if you want to use shared library(image,mhc) */
+#undef USE_SHARED
+
+/* Define if you want to use KAWARI module */
+#undef USE_KAWARI
+
+/* Define if you can use EUC hankaku-kana */
+
+#undef ENABLE_EUC_HANKAKU_KANA
